8c497395e8973df4520725c4bb13d0d1677659f4
Raphael_Benizio <raphael.benizio@medianesysteme.com>
2022-05-31 17:42:14 +0200

    Ajout barre de vie

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
